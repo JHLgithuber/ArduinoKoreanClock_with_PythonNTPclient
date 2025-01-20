40107c4e8a195ddc262b543fab7f8a9fdaf4833d
JHLgithuber <88205863+JHLgithuber@users.noreply.github.com>
2025-01-20 18:37:02 +0900

    프리셋 추가창 구현 프리셋 편집 리스트 헤더 전시불량

--- TimeOptimizer/gui.py
+++ TimeOptimizer/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from ttkthemes import ThemedTk
 from tkinter import ttk, messagebox, colorchooser
 import serial
 import json
@@ -16,11 +17,11 @@ class KoreanClockGUI:
         self.serial_connection = None  # 시리얼 연결 객체
 
         self.style = ttk.Style()
-        self.style.theme_use("clam")
-        self.style.configure("Accent.TButton", foreground="white", background="#0078D7", font=("Arial", 10))
+        self.style.theme_use("alt")
+        self.style.configure("Accent.TButton", foreground="white", background="lightgray", font=("Arial", 10))
         self.style.configure("Critical.TButton", foreground="white", font=("Arial", 10))
         self.style.map("Critical.TButton", background=[("active", "red"), ("!disabled", "maroon")])
-        self.style.configure("TCheckbutton", foreground="white", background="#0078D7", font=("Arial", 10))
+        self.style.configure("TCheckbutton", foreground="black", background="lightgray", font=("Arial", 10))
 
         self._create_widgets()
         self.root.mainloop()
@@ -99,10 +100,10 @@ class KoreanClockGUI:
         self.inner_time_preset_frame = ttk.Frame(self.time_preset_frame)
         self.inner_time_preset_frame.pack()
 
-        self.add_time_preset_button = ttk.Button(self.inner_time_preset_frame, text="프리셋 추가", command=self._open_add_preset_window)
+        self.add_time_preset_button = ttk.Button(self.inner_time_preset_frame, text="프리셋 추가", command=lambda: self._open_add_preset_window(apply_position=False))
         self.add_time_preset_button.grid(row=0, column = 0, columnspan = 2, sticky="EW", padx=3, pady=5, ipadx=5, ipady=5)
 
-        self.edit_time_preset_button = ttk.Button(self.inner_time_preset_frame, text="프리셋 편집", command=self._add_preset)
+        self.edit_time_preset_button = ttk.Button(self.inner_time_preset_frame, text="프리셋 편집", command=lambda: self._open_list_preset_window(apply_position=False))
         self.edit_time_preset_button.grid(row=1, column=0, sticky="EW", padx=3, pady=5, ipadx=5, ipady=5)
 
         self.reset_time_preset_button = ttk.Button(self.inner_time_preset_frame, text="프리셋 리셋", command=self._reset_preset, style="Critical.TButton")
@@ -119,7 +120,7 @@ class KoreanClockGUI:
         self.add_cust_preset_button = ttk.Button(self.inner_cust_preset_frame, text="프리셋 추가", command=self._open_add_preset_window)
         self.add_cust_preset_button.grid(row=0, column = 0, columnspan = 2, sticky="EW", padx=3, pady=5, ipadx=5, ipady=5)
 
-        self.edit_cust_preset_button = ttk.Button(self.inner_cust_preset_frame, text="프리셋 편집", command=self._add_preset)
+        self.edit_cust_preset_button = ttk.Button(self.inner_cust_preset_frame, text="프리셋 편집", command=self._open_list_preset_window)
         self.edit_cust_preset_button.grid(row=1, column=0, sticky="EW", padx=3, pady=5, ipadx=5, ipady=5)
 
         self.reset_cust_preset_button = ttk.Button(self.inner_cust_preset_frame, text="프리셋 리셋", command=self._reset_preset, style="Critical.TButton")
@@ -131,40 +132,198 @@ class KoreanClockGUI:
         )
         self.status_label.grid(row=2, column=0, columnspan=2, pady=10)
 
-    def _open_add_preset_window(self):
+    def _open_add_preset_window(self, apply_position=True, title="프리셋 추가"):
+        SUN_val = tk.BooleanVar()
+        MON_val = tk.BooleanVar()
+        TUE_val = tk.BooleanVar()
+        WED_val = tk.BooleanVar()
+        THU_val = tk.BooleanVar()
+        FRI_val = tk.BooleanVar()
+        SAT_val = tk.BooleanVar()
+
+        AM_val = tk.BooleanVar()
+        PM_val = tk.BooleanVar()
+        MID_val = tk.BooleanVar()
+        NOON_val = tk.BooleanVar()
+        H_val = tk.BooleanVar()
+        Hsuf_val = tk.BooleanVar()
+        M_val = tk.BooleanVar()
+        Msuf_val = tk.BooleanVar()
+
+        days_list = [
+            ["일요일", SUN_val],
+            ["월요일", MON_val],
+            ["화요일", TUE_val],
+            ["수요일", WED_val],
+            ["목요일", THU_val],
+            ["금요일", FRI_val],
+            ["토요일", SAT_val],
+        ]
+
+        position_list = [
+            ["오전", AM_val],
+            ["오후", PM_val],
+            ["자정", MID_val],
+            ["정오", NOON_val],
+            ["시", H_val],
+            ["시_접미사", Hsuf_val],
+            ["분", M_val],
+            ["분_접미사", Msuf_val],
+        ]
+
+        for _, day_val in days_list:
+            day_val = tk.BooleanVar()
+            day_val.set(True)
+
+        for _, position_val in position_list:
+            position_val = tk.BooleanVar()
+            position_val.set(True)
+
+
         """색상 프리셋 추가 창을 엽니다."""
-        self.add_preset_window= tk.Toplevel(self.root)
-        self.add_preset_window.title("프리셋 추가")
-        self.add_preset_window.geometry("600x350")
-        self.add_preset_window.resizable(False, False)
-        self.add_preset_window.configure(bg="#D9D9D9")
+        add_preset_window= tk.Toplevel(self.root)
+        add_preset_window.title(title)
+        add_preset_window.geometry("500x250")
+        add_preset_window.resizable(False, False)
+        add_preset_window.configure(bg="#D9D9D9")
 
-        self.add_preset_ledID_frame = ttk.LabelFrame(self.add_preset_window, text="적용 위치")
-        self.add_preset_ledID_frame.pack(fill="x", padx=5, pady=5, expand=True)
-        self.AM_checkbox=ttk.Checkbutton(self.add_preset_ledID_frame, text="오전", style="TCheckbutton")
-        self.AM_checkbox.pack(side="left")
-        self.PM_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="오후", style="TCheckbutton")
-        self.PM_checkbox.pack(side="left")
-        self.MID_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="자정", style="TCheckbutton")
-        self.MID_checkbox.pack(side="left")
-        self.NOON_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="정오", style="TCheckbutton")
-        self.NOON_checkbox.pack(side="left")
-        self.H_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="시", style="TCheckbutton")
-        self.H_checkbox.pack(side="left")
-        self.Hsuf_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="시_접미사", style="TCheckbutton")
-        self.Hsuf_checkbox.pack(side="left")
-        self.M_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="분", style="TCheckbutton")
-        self.M_checkbox.pack(side="left")
-        self.Msuf_checkbox = ttk.Checkbutton(self.add_preset_ledID_frame, text="분_접미사", style="TCheckbutton")
-        self.Msuf_checkbox.pack(side="left")
+        # Frame for input fields
+        add_preset_time_frame = ttk.LabelFrame(add_preset_window, text="적용 일시")
+        add_preset_time_frame.pack(fill="x", padx=5, pady=5, expand=True, side="top")
+        inner_add_preset_time_frame = ttk.Frame(add_preset_time_frame)
+        inner_add_preset_time_frame.pack()
 
-        self.add_preset_time_frame = ttk.Frame(self.add_preset_window)
-        self.add_preset_time_frame.pack()
+        # Start Time Comboboxes
+        start_hour = ttk.Combobox(inner_add_preset_time_frame, values=[f"{i:02}" for i in range(24)], width=3,
+                                  justify="center", state="readonly")
+        start_hour.set("00")
+        start_hour.grid(row=0, column=1, padx=2)
+
+        colon1 = ttk.Label(inner_add_preset_time_frame, text=":")
+        colon1.grid(row=0, column=2)
+
+        start_minute = ttk.Combobox(inner_add_preset_time_frame, values=[f"{i:02}" for i in range(0, 60, 10)], width=3,
+                                    justify="center", state="readonly")
+        start_minute.set("00")
+        start_minute.grid(row=0, column=3, padx=2)
+
+        label_from = ttk.Label(inner_add_preset_time_frame, text="~")
+        label_from.grid(row=0, column=4, padx=5)
+
+        # End Time Comboboxes
+        end_hour = ttk.Combobox(inner_add_preset_time_frame, values=[f"{i:02}" for i in range(24)], width=3, justify="center",
+                                state="readonly")
+        end_hour.set("00")
+        end_hour.grid(row=0, column=5, padx=2)
+
+        colon2 = ttk.Label(inner_add_preset_time_frame, text=":")
+        colon2.grid(row=0, column=6)
+
+        end_minute = ttk.Combobox(inner_add_preset_time_frame, values=[f"{i:02}" for i in range(0, 60, 10)], width=3,
+                                  justify="center", state="readonly")
+        end_minute.set("00")
+        end_minute.grid(row=0, column=7, padx=2)
+
+        inner_add_preset_day_frame = ttk.Frame(add_preset_time_frame)
+        inner_add_preset_day_frame.pack()
 
 
 
+        for i, (text, variable) in enumerate(days_list):
+            checkbox = ttk.Checkbutton(
+                inner_add_preset_day_frame,
+                text = text,
+                style = "TCheckbutton",
+                variable = variable,
+                onvalue = True,
+                offvalue = False,
+            )
+            checkbox.grid(row=0, column=i, padx=3, pady=5)
 
 
+        if apply_position:
+            add_preset_ledID_frame = ttk.LabelFrame(add_preset_window, text="적용 위치")
+            add_preset_ledID_frame.pack(fill="x", padx=5, pady=5, expand=True, side="top")
+            inner_preset_ledID_frame = ttk.Frame(add_preset_ledID_frame)
+            inner_preset_ledID_frame.pack()
+
+            for i, (text, variable) in enumerate(position_list):
+                checkbox = ttk.Checkbutton(
+                    inner_preset_ledID_frame,
+                    text=text,
+                    style="TCheckbutton",
+                    variable=variable,
+                    onvalue=True,
+                    offvalue=False,
+                )
+                checkbox.grid(row=0, column=i, padx=3, pady=5)
+
+        add_preset_color_frame = ttk.Frame(add_preset_window)
+        add_preset_color_frame.pack(side="top", padx=5, pady=5)
+
+        color_preview_canvas = tk.Canvas(add_preset_color_frame, width=30, height=30, bg="white")
+        color_preview_canvas.grid(row=0, column=0, padx=10, pady=10)
+
+        choose_color_button = ttk.Button(add_preset_color_frame, text="색상 선택", command=self._choose_color)
+        choose_color_button.grid(row=0, column=1, pady=10, ipadx=10, ipady=5)
+
+
+        # Submit Button
+        submit_button = ttk.Button(add_preset_window, text="저장")
+        submit_button.pack(side="bottom", ipadx=15, ipady=5, padx=5, pady=5)
+
+    def _open_list_preset_window(self, apply_position=True, title="프리셋 편집"):
+        self.list_preset_window = tk.Toplevel(self.root)
+        self.list_preset_window.title(title)
+        self.list_preset_window.geometry("500x200")
+        self.list_preset_window.resizable(True, True)
+        self.list_preset_window.configure(bg="#D9D9D9")
+
+        # 기본 컬럼 정의
+        default_columns = ("priority", "color", "start_time", "end_time", "dayOfWeek")
+        table = ttk.Treeview(
+            self.list_preset_window, columns=default_columns, show="headings"
+        )
+
+        #TODO: 리스트 헤더 안나옴
+
+        # 헤더 텍스트 설정
+        column_headers = {
+            "priority": "우선순위",
+            "color": "색상",
+            "start_time": "시작 시간",
+            "end_time": "종료 시간",
+            "dayOfWeek": "요일",
+            "AM": "오전",
+            "PM": "오후",
+            "MID": "자정",
+            "NOON": "정오",
+            "H": "시",
+            "Hsuf": "시_접미사",
+            "M": "분",
+            "Msuf": "분_접미사",
+            "delete_button": "삭제",
+        }
+
+        # 기본 컬럼 설정
+        for column in default_columns:
+            table.heading(column, text=column_headers.get(column, column))
+            table.column(column, anchor="center", width=10)
+
+        # 추가 컬럼 설정 (if apply_position=True)
+        if apply_position:
+            additional_columns = ("AM", "PM", "MID", "NOON", "H", "Hsuf", "M", "Msuf")
+            table["columns"] += additional_columns
+            for column in additional_columns:
+                table.heading(column, text=column_headers.get(column, column))
+                table.column(column, anchor="center", width=10)
+
+        # 삭제 버튼 추가
+        table["columns"] += ("delete_button",)
+        table.heading("delete_button", text=column_headers.get("delete_button", None))
+        table.column("delete_button", anchor="center", width=10)
+
+        table.pack(expand=True, fill="both")
 
     def _open_manual_time_window(self):
         """수동 시간 설정 창을 엽니다."""
@@ -181,24 +340,24 @@ class KoreanClockGUI:
         # 시간 입력 위젯
         self.hour_label = ttk.Label(self.manual_window, text="시:")
         self.hour_label.grid(row=1, column=0, padx=5, pady=5)
-        self.hour_entry = ttk.Entry(self.manual_window, width=5, font=("Arial", 10))
+        self.hour_entry = ttk.Entry(self.manual_window, width=5, font=("Arial", 10), justify="center")
         self.hour_entry.insert(0, "12")
         self.hour_entry.grid(row=1, column=1, padx=5, pady=5)
 
         self.minute_label = ttk.Label(self.manual_window, text="분:")
         self.minute_label.grid(row=2, column=0, padx=5, pady=5)
-        self.minute_entry = ttk.Entry(self.manual_window, width=5, font=("Arial", 10))
+        self.minute_entry = ttk.Entry(self.manual_window, width=5, font=("Arial", 10), justify="center")
         self.minute_entry.insert(0, "00")
         self.minute_entry.grid(row=2, column=1, padx=5, pady=5)
 
         self.second_label = ttk.Label(self.manual_window, text="초:")
         self. second_label.grid(row=3, column=0, padx=5, pady=5)
-        self.second_entry = ttk.Entry(self.manual_window, width=5, font=("Arial", 10))
+        self.second_entry = ttk.Entry(self.manual_window, width=5, font=("Arial", 10), justify="center")
         self.second_entry.insert(0, "00")
         self.second_entry.grid(row=3, column=1, padx=5, pady=5)
 
         def set_manual_time():
-            self.selected_date = calendar.get_date()
+            self.selected_date = self.calendar.get_date()
             self.hour = self.hour_entry.get()
             self.minute = self.minute_entry.get()
             self.second = self.second_entry.get()
